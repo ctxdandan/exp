d82e8ab90f336887d6b201315f091b20ee9c3110
zhangjunhua <2414890918@qq.com>
2018-04-01 16:55:13 +0800

    clear old code, next is to refactor the code

--- lib/scc.h
+++ lib/scc.h
@@ -22,18 +22,6 @@ namespace scc{
     vector<int> new_stack;
     int time=0;
 
-    struct node {
-        int v,next;
-    }edge[1001];
-    int DFN[1001],LOW[1001];
-    int stack[1001],heads[1001],visit[1001],cnt=0,tot=0,index=0;
-
-    void add(int x,int y)
-    {
-        edge[++cnt].next=heads[x];
-        edge[cnt].v = y;
-        heads[x]=cnt;
-    }
     void tarjan2(int vid){
         time++;
         vertexes[vid].DFN=vertexes[vid].LOW=time;
@@ -62,32 +50,7 @@ namespace scc{
             }
         }
     }
-    void tarjan(int x)//代表第几个点在处理。递归的是点。
-    {
-        DFN[x]=LOW[x]=++tot;// 新进点的初始化。
-        stack[++index]=x;//进站
-        visit[x]=1;//表示在栈里
-        for(int i=heads[x];i!=-1;i=edge[i].next)
-        {
-            if(!DFN[edge[i].v]) {//如果没访问过
-                tarjan(edge[i].v);//往下进行延伸，开始递归
-                LOW[x]=min(LOW[x],LOW[edge[i].v]);//递归出来，比较谁是谁的儿子／父亲，就是树的对应关系，涉及到强连通分量子树最小根的事情。
-            }
-            else if(visit[edge[i].v ]){  //如果访问过，并且还在栈里。
-                LOW[x]=min(LOW[x],DFN[edge[i].v]);//比较谁是谁的儿子／父亲。就是链接对应关系
-            }
-        }
-        if(LOW[x]==DFN[x]) //发现是整个强连通分量子树里的最小根。
-        {
-            do{
-                printf("%d ",stack[index]);
-                visit[stack[index]]=0;
-                index--;
-            }while(x!=stack[index+1]);//出栈，并且输出。
-            printf("\n");
-        }
-        return ;
-    }
+
     int test()
     {
         streambuf *backup;
@@ -102,7 +65,6 @@ namespace scc{
 // ... cin will read from file
 //    cin.rdbuf(backup);     // restore cin's original streambuf
 
-        memset(heads,-1,sizeof(heads));
         int n,m;//n nodes and m edges
         cin>>n>>m;
         vertexes.resize(n);
@@ -113,22 +75,9 @@ namespace scc{
         {
             cin>>x>>y;
             edges[x-1].push_back(y-1);
-            add(x,y);
-        }
-        for(int i=0;i<10;i++)
-        {
-            printf("Edge[%d]{v:%d, next:%d}\n",i,edge[i].v,edge[i].next);
         }
 
-        for(int i=0;i<10;i++)
-        {
-            printf("heads[%d]%d\n",i,heads[i]);
-        }
 
-        for(int i=1;i<=n;i++) {
-            if (!DFN[i]) tarjan(i);//当这个点没有访问过，就从此点开始。防止图没走完
-        }
-        printf("output of new algorithm\n");
         for(int i=0;i<vertexes.size();i++){
             if(!vertexes[i].DFN) tarjan2(i);
         }
