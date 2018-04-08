a5072675e6e96c9a05c3847186fe386d81e4999a
zhangjunhua <2414890918@qq.com>
2018-04-08 17:51:04 +0800

    run on labMachine
    Signed-off-by: zhangjunhua <2414890918@qq.com>

--- tol.cpp
+++ tol.cpp
@@ -10,6 +10,19 @@ double get_current_time()
     return (double)t.tv_sec + (double)t.tv_usec / 1000000;
 }
 
+void sstreamtest(){
+    istringstream iss("I'm a c++ lover.");
+    ostringstream oss;
+    string s;
+    while(iss>>s){
+        oss<<s<<" ";
+        cout<<s<<endl;
+    }
+    cout<<oss.str()<<endl;
+    printf("%s cstr",oss.str().c_str());
+
+
+}
 
 int main() {
     double start_time=get_current_time();
@@ -19,7 +32,12 @@ int main() {
     double end_time=get_current_time();
     log("start_time:%f s end_time:%f s duration:%f s",start_time,end_time,end_time-start_time);
     return 0;
+//    sstreamtest();
 }
 
-//[C:\Users\Admin\CLionProjects\tol\tol.cpp.main()#20]
+//[C:\Users\Admin\CLionProjects\tol\tol.cpp.main()#20]# old laptop
 // start_time:1522608723.080438 s end_time:1522608738.064266 s duration:14.983828 s
+
+//[C:\Users\admin\CLionProjects\exp\tol.cpp.main()#22]# computer in lab
+// start_time:1522632450.765156 s end_time:1522632456.914473 s duration:6.149317 s
+
