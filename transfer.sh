8ac449c35c9a1f006a6ebfb3f73b3b8fb534c6cf
Junhua Zhang <2414890918@qq.com>
2018-08-07 22:57:44 +0800

    add O3 Optimization

--- transfer.sh
+++ transfer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 scp ~/CLionProjects/exp/tol.cpp n1:~/exp/
 scp -r ~/CLionProjects/exp/lib n1:~/exp/
-ssh n1 "cd ~/exp;g++ tol.cpp -o graphTool;echo 'compile succeed'"
+ssh n1 "cd ~/exp;g++ -O3 tol.cpp -o graphTool;echo 'compile succeed'"
 echo "transfer succeed"
